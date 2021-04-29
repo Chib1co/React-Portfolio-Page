c730cea383cd992753f75fa1d8474c385464d641
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2021-04-29 13:54:35 +1000

    fixing image file src

--- portfolio/src/pages/Portfolio.js
+++ portfolio/src/pages/Portfolio.js
@@ -12,7 +12,7 @@ export default function Portfolio() {
         {
             title: "Gym Tracker/Group project2",
             desc:  "Second group project. Also out first full stack web application.  We created fake gym management app. ",
-            img: "/images/Gym_tracker.png",
+            img: process.env.PUBLIC_URL + "/images/Gym_tracker.png",
             img_alt: "gymtracker-project2",
             github: "https://github.com/Chib1co/gymtrack",        
             app_link: "https://gymtrack-app.herokuapp.com/",
@@ -21,7 +21,7 @@ export default function Portfolio() {
         {
             title: "Note taker",
             desc:  "Using Node.JS express. Simple note taking application. Keep the note, edit and delete. ",
-            img: "/images/Note_taker.png",
+            img: process.env.PUBLIC_URL + "/images/Note_taker.png",
             img_alt: "Note tracker",
             github: "https://github.com/Chib1co/Your-Note-Take",
             app_link: "https://limitless-depths-93347.herokuapp.com/",
@@ -30,7 +30,7 @@ export default function Portfolio() {
         {
             title: "Weather WeatherForecast_Yourcity",
             desc:  "Check the current weather of the searced city, also next 5 days weather forecast. local storage storesthe search history up to 5 cities.",
-            img: "/images/weather-repo.png",
+            img: process.env.PUBLIC_URL + "/images/weather-repo.png",
             img_alt: "Weather forecast",
             github: "https://github.com/Chib1co/WeatherForecast_Yourcity",
             app_link: "https://chib1co.github.io/WeatherForecast_Yourcity/",
@@ -39,7 +39,7 @@ export default function Portfolio() {
         {
             title: "Code quiz",
             desc:  "first web application used Javascript. You answer the quiz and save the score in your local storage.",
-            img: "/images/codequiz1.png",
+            img: process.env.PUBLIC_URL + "/images/codequiz1.png",
             img_alt: "code quiz",
             github: "https://github.com/Chib1co/Fun_CodeQuiz",
             app_link: "https://chib1co.github.io/Fun_CodeQuiz/",
@@ -48,7 +48,7 @@ export default function Portfolio() {
         {
             title: "Restaurant Otaku/Group project1",
             desc:  "First group project. Used zomato API and google map API to search restaurants and add personal reviews and photos",
-            img: "/images/project1.png",
+            img: process.env.PUBLIC_URL + "/images/project1.png",
             img_alt: "project1/restaurant otaku",
             github: "https://github.com/Chib1co/Covid-Safe-Restaurant",
             app_link: "https://chib1co.github.io/Covid-Safe-Restaurant/",
@@ -57,7 +57,7 @@ export default function Portfolio() {
         {
             title: "Burger",
             desc:  "Using mysql. ORM and handlebars, express. Adding burger, move to devoured whn you click the button.",
-            img: "/images/burger_demoPage.png",
+            img: process.env.PUBLIC_URL + "/images/burger_demoPage.png",
             img_alt: "burger",
             github: "https://github.com/Chib1co/Burger",
             // app_link: "https://chib1co.github.io/Covid-Safe-Restaurant/",
